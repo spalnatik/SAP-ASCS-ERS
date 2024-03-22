ea96b6f3e6e41c0d814b4a01fd85565c9b0e334f
spalnatik <139609488+spalnatik@users.noreply.github.com>
2024-03-22 21:13:18 +0530

    Update softlinks.sh

--- softlinks.sh
+++ softlinks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-service autofs reload
+service autofs restart
 	
 cd /usr/sap/NW1/SYS/
 
 ln -s /sapmnt/NW1/global global
 ln -s /sapmnt/NW1/profile profile
-crm resource clean
+crm resource cleanup
